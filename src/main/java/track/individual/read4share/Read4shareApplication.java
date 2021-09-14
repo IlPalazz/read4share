32786105c50497f1e7a6903cc882cd5e332872b3
IlPalazz <fede.palazzi1999@gmail.com>
2021-09-14 09:30:48 +0200

    Initial project structure

---
+++ src/main/java/track/individual/read4share/Read4shareApplication.java
@@ -0,0 +1,13 @@
+package track.individual.read4share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Read4shareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Read4shareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/track/individual/read4share/Read4shareApplicationTests.java
@@ -0,0 +1,13 @@
+package track.individual.read4share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Read4shareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
